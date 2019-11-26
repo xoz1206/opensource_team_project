6dfb4b480fd1533c78e041ed2f59a64829453c29
sumin <ism0705@naver.com>
2019-11-26 08:26:19 +0000

    final version 1.2

--- camera.sh
+++ camera.sh
@@ -1,2 +1,4 @@
 python3 red_ball_tracing_camera_version.py
 #continue
+# infinite loop
+# ctrl + c
--- transfer.sh
+++ transfer.sh
@@ -2,11 +2,11 @@
 
 while [ 1 ]
 do
-    read -t 0.1 answer
+    read -t 0.05 answer
     sftp robot@10.42.0.3 << !
     put red_ball_info.txt
 !
-    if [ "$answer" == "q" ]; then
+    if [ "$answer" == "Q" ]; then
         exit
     fi
 done
